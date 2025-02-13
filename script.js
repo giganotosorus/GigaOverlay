3f6a5501f958cccedbc13cda44335c009e888e83
giganotosorus <79913978+giganotosorus@users.noreply.github.com>
2025-02-13 14:32:56 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         GigaOverlayV2
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Un overlay permettant d'avoir des informations sur le jeux et des fonctionnalités en plus
+// @author       giganotosorus mort
+// @match        https://jklm.fun/*
+// @match        https://*.jklm.fun/games/bombparty/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jklm.fun
+// ==/UserScript==
+
+if (window === window.top) {
+    
+} else {
+
+}
