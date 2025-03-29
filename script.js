2b7e9c8c615cac5b59e9725c0c7897506ae5a6a2
giganotosorus <79913978+giganotosorus@users.noreply.github.com>
2025-03-29 03:28:43 +0100

    Allow the overlay to load in popsauce

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // @description  Un overlay permettant d'avoir une liste d'amis et quelques fonctionnalités supplémentaires sur jklm.fun
 // @author       giganotosorus mort
 // @match        https://jklm.fun/*
-// @match        https://*.jklm.fun/games/bombparty/
+// @match        https://*.jklm.fun/games/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=jklm.fun
 // @grant       GM_xmlhttpRequest
 // @connect     gigaoverlay.online
