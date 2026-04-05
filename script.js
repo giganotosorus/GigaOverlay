1143238349101e201b04e3fa40cbb0a785267b1d
giganotosorus <giganotosorus.mort@gmail.com>
2026-04-05 23:35:30 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GigaOverlay
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.1.1
 // @description  Un overlay permettant d'avoir une liste d'amis et quelques fonctionnalités supplémentaires sur jklm.fun
 // @author       giganotosorus mort
 // @match        https://jklm.fun/*
@@ -10,12 +10,12 @@
 // @updateURL    https://raw.githubusercontent.com/giganotosorus/GigaOverlay/refs/heads/main/script.js
 // @downloadURL  https://raw.githubusercontent.com/giganotosorus/GigaOverlay/refs/heads/main/script.js
 // @grant        GM_xmlhttpRequest
-// @connect      giganotosorus.ddns.net
+// @connect      giganotosorus.eu
 // ==/UserScript==
 
 const roomCode = location.pathname.substr(1, 4).toUpperCase();
 
-const url = "https://giganotosorus.ddns.net/api/overlay?file=" + (!roomCode ? "overlay_home" : window.top === window.self ? "overlay_room" : "overlay_game");
+const url = "https://giganotosorus.eu/api/overlay?file=" + (!roomCode ? "overlay_home" : window.top === window.self ? "overlay_room" : "overlay_game");
 unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest;
 
 $make('script', document.body, { src: url });
